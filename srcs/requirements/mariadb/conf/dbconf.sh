828b7e24a013ea8a2f833bb73d8f44234fbc8ff5
Mohamed Ouaammou <moouaamm@e3r4p15.1337.ma>
2024-06-09 02:38:38 +0100

    done

---
+++ srcs/requirements/mariadb/conf/dbconf.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+service mariadb start
+
+sleep 5
+
+
+
+
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > file
+
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';" >> file
+
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> file
+
+echo "FLUSH PRIVILEGES;" >> file
+
+mysql  < file
+
+service mariadb stop
+
+mysqld_safe
